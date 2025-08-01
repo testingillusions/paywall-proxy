b813ca805e8ef8cd4e08d61d3ad4770a842b8e17
Joe Wood <joe.kayak@gmail.com>
2025-07-31 21:08:22 -0400

    Update to Healthcheck

--- routes/health.js
+++ routes/health.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/healthcheck', (req, res) => res.send('OK'));
+router.get('/', (req, res) => res.send('OK'));
 
 module.exports = router;
