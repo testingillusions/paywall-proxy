34c4609f05720d6c0dbe4b792793b23329742da3
Joe Wood <joe.kayak@gmail.com>
2025-07-31 22:54:52 -0400

    db proxy

--- routes/proxy.js
+++ routes/proxy.js
@@ -4,12 +4,13 @@ const zlib = require('zlib');
 const { DISALLOWED_EXTENSIONS = [] } = {};
 
 module.exports = createProxyMiddleware({
+  console.log('🔁 Proxying request to IN MIDDLEWARE:', req.url);
   target: targetUrl,
   changeOrigin: true,
   ws: true,
   //pathRewrite: (path) => path,
   onProxyReq: (proxyReq, req) => {
-    onsole.log('🔁 Proxying request to:', req.url);
+    console.log('🔁 Proxying request to:', req.url);
     if (req.user?.email) proxyReq.setHeader('VUE-EMAIL', req.user.email);
     proxyReq.setHeader('VUE-AUTH', 'AE8A774F-1DE0-4F98-B037-659645706A66'); 
     proxyReq.setHeader('TBA-PLAN-TIER', 'Tier1');  
