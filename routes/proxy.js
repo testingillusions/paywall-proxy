a44cb36d913562189dbb93107edf766a868d0514
Joe Wood <joe.kayak@gmail.com>
2025-07-31 22:30:06 -0400

    db Proxy with headers

--- routes/proxy.js
+++ routes/proxy.js
@@ -10,6 +10,9 @@ module.exports = createProxyMiddleware({
   pathRewrite: (path) => path,
   onProxyReq: (proxyReq, req) => {
     if (req.user?.email) proxyReq.setHeader('VUE-EMAIL', req.user.email);
+    proxyReq.setHeader('VUE-AUTH', 'AE8A774F-1DE0-4F98-B037-659645706A66'); 
+    proxyReq.setHeader('TBA-PLAN-TIER', 'Tier1');  
+
   },
   onProxyRes: (proxyRes, req, res) => {
     // minimal rewrite logic
