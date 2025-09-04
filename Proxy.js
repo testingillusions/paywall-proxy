a69c567249429856734efa7ea95efe74ec12eceb
Joe Wood <joe.kayak@gmail.com>
2025-09-03 21:43:30 -0400

    Fixed CORS issue by correcting the allowed origin URL.

--- Proxy.js
+++ Proxy.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
     const origin = req.headers.origin;
 
     // Update this if supporting multiple origins
-    const allowedOrigin = 'http://localhost.com';
+    const allowedOrigin = 'http://localhost:8080';
 
     if (origin === allowedOrigin) {
         res.header('Access-Control-Allow-Origin', allowedOrigin);
